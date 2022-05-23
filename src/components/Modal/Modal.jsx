36605163db950977b041d0a84d41e4702b5e3c07
Olena Voitsekhivska <mirkbog@gmail.com>
2022-05-23 14:54:52 +0300

    close on backdrop click

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,42 +2,41 @@ import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import s from './Modal.module.css';
 import PropTypes from 'prop-types';
-// import Loader from 'components/Loader/Loader';
-
 
 const modalRoot = document.querySelector('#modalRoot');
 
-
-export default function Modal ({onClose, pic}){
-
-  useEffect(()=>{
+export default function Modal({ onClose, pic }) {
+  useEffect(() => {
     const handleKeyDown = e => {
       if (e.code === 'Escape') {
         return onClose();
       }
     };
+    const handleClickAway = e => {
+      if (e.target.className.includes('Modal_overlay')) {
+        return onClose();
+      }
+    };
 
     window.addEventListener('keydown', handleKeyDown);
-    return ()=>{
+    window.addEventListener('click', handleClickAway);
+    return () => {
       window.removeEventListener('keydown', handleKeyDown);
-    }
-  })
+      window.removeEventListener('click', handleClickAway);
+    };
+  });
 
- 
-    return createPortal(
-      <div className={s.overlay}>
-        <div className={s.modal}>
-     <img src={pic} alt="" />  
-
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-
-
+  return createPortal(
+    <div className={s.overlay}>
+      <div className={s.modal}>
+        <img src={pic} alt="" />
+      </div>
+    </div>,
+    modalRoot
+  );
+}
 
 Modal.propTypes = {
-  onClose:PropTypes.func.isRequired,
-  pic:PropTypes.string.isRequired
-}
+  onClose: PropTypes.func.isRequired,
+  pic: PropTypes.string.isRequired,
+};
